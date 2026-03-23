4b0637c5130052f12813467d936fa7e2cf08a410
meron-23 <meronmuluye75@gmail.com>
2026-03-23 04:28:26 -0700

    Delete app/models/__init__.py

--- app/models/__init__.py
+++
@@ -1,1 +0,0 @@
-from app.models.models import User, UserProfile, LegalDocument, DocumentSection, Category, DocumentCategory, SavedItem, SearchHistory, DownloadLog, StudentVerification, UploadJob, Admin
